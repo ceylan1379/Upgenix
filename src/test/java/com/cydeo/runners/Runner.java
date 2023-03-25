eb1d6bd1933a12ba06e478a3273ed6d09517d1d4
WİN10 <gumusceylan147@gmail.com>
2023-03-25 13:40:12 +0100

    some change

--- src/test/java/com/cydeo/runners/Runner.java
+++ src/test/java/com/cydeo/runners/Runner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         glue ="com/cydeo/step_definitions",
         dryRun=false,
         tags = "@smoke",
-        publish=true
+        publish=false
 )
 
 public class Runner {
