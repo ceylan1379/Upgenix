c3a7bd449c713f0b7539b04942156f3cf669b556
WİN10 <gumusceylan147@gmail.com>
2023-03-25 13:45:09 +0100

    add jenkins file

--- src/test/java/com/cydeo/runners/Runner.java
+++ src/test/java/com/cydeo/runners/Runner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         glue ="com/cydeo/step_definitions",
         dryRun=false,
         tags = "@smoke",
-        publish=false
+        publish=true
 )
 
 public class Runner {
